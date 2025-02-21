0293ddb409795866b032904e4b128384a07421e3
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2025-02-21 19:01:55 +0200

    Day 6 solution

--- src/AOC2024D6.hs
+++ src/AOC2024D6.hs
@@ -1,9 +1,12 @@
 module AOC2024D6 (solution, part1, part2) where
 
 import           Control.Arrow ((&&&))
+import qualified Control.Monad
 import qualified Data.Array    as A
 import qualified Data.Set      as S
 import           Debug.Trace
+import           Linear.V2     (V2 (..), _y, perp)
+import           Linear.Vector (unit)
 
 data Entity = Empty | Obstacle deriving (Eq, Show)
 
@@ -11,9 +14,9 @@ type Room = A.Array Coord Entity
 
 type Input = (Guard, Room)
 
-type Coord = (Int, Int)
+type Coord = V2 Int
 
-data Direction = N | E | S | W deriving (Enum, Eq, Ord, Show)
+type Direction = V2 Int
 
 data Guard = Guard { dir :: Direction
                    , pos :: Coord
@@ -24,15 +27,14 @@ charToEntity :: Char -> Entity
 charToEntity '#' = Obstacle
 charToEntity _   = Empty
 
+north :: Direction
+north = negate . unit $ _y
+
 turn :: Direction -> Direction
-turn W = N
-turn d = succ d
+turn = perp
 
 move :: Direction -> Coord -> Coord
-move N (x, y) = (x, y - 1)
-move E (x, y) = (x + 1, y)
-move S (x, y) = (x, y + 1)
-move W (x, y) = (x - 1, y)
+move d c = d + c
 
 inGrid :: Room -> Coord -> Bool
 inGrid = A.inRange . A.bounds
@@ -41,32 +43,32 @@ step :: Room -> Guard -> Guard
 step r (Guard d c) = Guard d' (move d' c)
   where
     d' = head . filter isEmpty . iterate turn $ d
-    isEmpty d'' = not (inGrid r p') || r A.! p' == Empty
+    isEmpty d'' = not (inGrid r p' && r A.! p' == Obstacle)
       where
         p' = move d'' c
 
 getInput :: IO Input
 getInput = do
   i <- lines <$> readFile "./input/day6"
-  let indexed = concat (zipWith (\c l -> zipWith (\r s -> ((r, c), s)) [1 ..] l) [1 ..] i)
-  let guard = Guard N (head [p | (p, '^') <- indexed])
-  pure (guard, A.array ((1, 1), (length . head $ i, length i)) (map (fmap charToEntity) indexed))
+  let indexed = concat (zipWith (\c l -> zipWith (\r s -> (V2 r c, s)) [1 ..] l) [1 ..] i)
+  let guard = Guard north (head [p | (p, '^') <- indexed])
+  pure (guard, A.array (V2 1 1, V2 (length . head $ i) (length i)) (map (fmap charToEntity) indexed))
 
 part1 :: Input -> Int
 part1 (g, r) = S.size . S.fromList . takeWhile (inGrid r) . map pos . iterate (step r) $ g
 
 part2 :: Input -> Int
-part2 (g, r) =
-  let obstacles = S.toList . S.fromList . filter (/= pos g) . takeWhile (inGrid r) . map pos . iterate (step r) $ g
-   in length . filter id $ do
-        o <- obstacles
-        let r' = r A.// [(o, Obstacle)]
-        pure . isLoop $ r'
+part2 (g, r) = length . filter id $ do
+  o <- obstacles
+  Control.Monad.guard (o /= pos g)
+  let r' = r A.// [(o, Obstacle)]
+  pure . isLoop $ r'
   where
-    isLoop r' = go S.empty (takeWhile (inGrid r' . pos) . iterate (step r') $ g)
+    obstacles = S.toList . S.fromList . filter (/= pos g) . takeWhile (inGrid r) . map pos . iterate (step r) $ g
+    isLoop r' = go S.empty (takeWhile (inGrid r' . pos) (iterate (step r') g))
     go _ [] = False
     go seen (g : gs)
-      | elem g seen = True
+      | S.member g seen = True
       | otherwise = go (S.insert g seen) gs
 
 solution :: IO (Int, Int)
