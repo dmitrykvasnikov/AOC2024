8f62493a626728ca8ced7a1bcc5548c5aca7e210
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2025-02-26 14:44:38 +0200

    minor fixes

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import qualified AOC2024D3 as D3
 import qualified AOC2024D4 as D4
 import qualified AOC2024D5 as D5
 import qualified AOC2024D6 as D6
+import qualified AOC2024D7 as D7
 import           System.IO
 
 getHandler :: String -> Maybe (IO (Int, Int))
@@ -15,6 +16,7 @@ getHandler "3" = Just $ D3.solution
 getHandler "4" = Just $ D4.solution
 getHandler "5" = Just $ D5.solution
 getHandler "6" = Just $ D6.solution
+getHandler "7" = Just $ D7.solution
 getHandler _   = Nothing
 
 main :: IO ()
--- src/AOC2024D7.hs
+++ src/AOC2024D7.hs
@@ -1,19 +1,51 @@
 module AOC2024D7 (solution, part1, part2) where
 
-import           Control.Arrow ((&&&))
+import           Control.Arrow                 ((&&&))
+import           Text.Regex.Applicative        (RE (..), many, match, string,
+                                                sym)
+import           Text.Regex.Applicative.Common (decimal)
 
-type Input = String
+type Calibration = Int
+
+type Operand = Int
+
+type Operation = Operand -> Operand -> Operand
+
+type Input = [(Calibration, [Operand])]
+
+type Parser = RE Char
+
+calibration :: Parser Calibration
+calibration = decimal
+
+operands :: Parser [Operand]
+operands = (:) <$> decimal <*> many (sym ' ' *> decimal)
+
+operations_part1 :: [Operation]
+operations_part1 = [(+), (*)]
+
+operations_part2 :: [Operation]
+operations_part2 = [(+), (*), (\x y -> read $ show x <> show y)]
+
+input :: Parser Input
+input = many ((,) <$> (calibration <* string ": ") <*> (operands <* sym '\n'))
 
 getInput :: IO Input
-getInput = do
-  i <- readFile "./input/day7"
-  pure undefined
+getInput = (maybe [] id . match input) <$> readFile "./input/day7"
+
+decode :: [Operation] -> Calibration -> [Operand] -> Bool
+decode operations cal ops = any id (go (head ops) (tail ops))
+  where
+    go res [] = [res == cal]
+    go res (o : ops)
+      | res >= cal = [False]
+      | otherwise = concatMap (flip go ops) (map (\op -> op res o) operations)
 
 part1 :: Input -> Int
-part1 i = 0
+part1 = sum . map fst . filter (uncurry (decode operations_part1))
 
 part2 :: Input -> Int
-part2 i = 0
+part2 = sum . map fst . filter (uncurry (decode operations_part2))
 
 solution :: IO (Int, Int)
 solution = getInput >>= pure . (part1 &&& part2)
