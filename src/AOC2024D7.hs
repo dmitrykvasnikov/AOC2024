08fdaaf026376d702208693d1ea7981e4ca4fa6d
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2025-02-27 16:37:06 +0200

    Day 7 solution

--- src/AOC2024D7.hs
+++ src/AOC2024D7.hs
@@ -1,51 +1,57 @@
 module AOC2024D7 (solution, part1, part2) where
 
 import           Control.Arrow                 ((&&&))
-import           Text.Regex.Applicative        (RE (..), many, match, string,
-                                                sym)
+import           Text.Regex.Applicative        (RE (..), match, some, sym)
 import           Text.Regex.Applicative.Common (decimal)
 
-type Calibration = Int
+data Equation a = Equation a [a]
+  deriving (Show)
 
-type Operand = Int
+data Operator = Add | Mult | Concat deriving (Show)
 
-type Operation = Operand -> Operand -> Operand
+-- Solution has a resulting value, first element and list of operators with second operand
+data Solution a = Solution a a [(a, Operator)]
+  deriving (Show)
 
-type Input = [(Calibration, [Operand])]
+type Input = [Equation Int]
 
 type Parser = RE Char
 
-calibration :: Parser Calibration
-calibration = decimal
-
-operands :: Parser [Operand]
-operands = (:) <$> decimal <*> many (sym ' ' *> decimal)
-
-operations_part1 :: [Operation]
-operations_part1 = [(+), (*)]
-
-operations_part2 :: [Operation]
-operations_part2 = [(+), (*), (\x y -> read $ show x <> show y)]
-
-input :: Parser Input
-input = many ((,) <$> (calibration <* string ": ") <*> (operands <* sym '\n'))
+equation :: Parser (Equation Int)
+equation = Equation <$> (decimal <* sym ':') <*> (some (sym ' ' *> decimal))
 
 getInput :: IO Input
-getInput = (maybe [] id . match input) <$> readFile "./input/day7"
+getInput = maybe (error "parse error") id . match (some (equation <* sym '\n')) <$> readFile "./input/day7"
 
-decode :: [Operation] -> Calibration -> [Operand] -> Bool
-decode operations cal ops = any id (go (head ops) (tail ops))
+apply :: (Num a, Show a, Read a) => Operator -> a -> a -> a
+apply Add    = (+)
+apply Mult   = (*)
+apply Concat = \x y -> read (show x <> show y)
+
+solutions :: [Operator] -> Equation Int -> [Solution Int]
+solutions ops eq@(Equation goal (num : nums)) = Solution goal num <$> go num nums
   where
-    go res [] = [res == cal]
-    go res (o : ops)
-      | res >= cal = [False]
-      | otherwise = concatMap (flip go ops) (map (\op -> op res o) operations)
+    go total nums' = case (compare total goal, nums') of
+      (GT, _) -> []
+      (LT, []) -> []
+      (EQ, []) -> [[]]
+      (_, (x : xs)) -> do
+        op <- ops
+        ((x, op) :) <$> go (apply op total x) xs
+
+solve :: [Operator] -> Input -> Int
+solve ops = sum . map score
+  where
+    score :: (Equation Int) -> Int
+    score eq@(Equation goal _)
+      | null (solutions ops eq) = 0
+      | otherwise = goal
 
 part1 :: Input -> Int
-part1 = sum . map fst . filter (uncurry (decode operations_part1))
+part1 = solve [Mult, Add]
 
 part2 :: Input -> Int
-part2 = sum . map fst . filter (uncurry (decode operations_part2))
+part2 = solve [Mult, Concat, Add]
 
 solution :: IO (Int, Int)
 solution = getInput >>= pure . (part1 &&& part2)
